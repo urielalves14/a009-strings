f91ca5b62392306ad2755815dc9d4429b1eacf6f
urielalves14 <alvesuriel57@gmail.com>
2022-08-31 23:30:12 -0300

    atividadestrings

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+//1- Faça um programa que pergunte ao usuário seu nome, e suas 3 comidas favoritas.
+
+const nomeUsuario = prompt("Qual o seu nome?")
+let comida1 = prompt("Qual a sua comida favorita?")
+let comida2 = prompt("Agora fala outra.")
+let comida3 = prompt("E a ultima?")
+
+//Você deve usar apenas um `console.log()` para isso
+
+console.log(`Estas são as comidas favoritas de : ${nomeUsuario}\n- ${comida1}\n- ${comida2}\n- ${comida3}`)
